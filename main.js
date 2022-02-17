84899516edda702f6c8f8ef03780f8435383a91b
Hernan Mele <hernan.mele@gmail.com>
2022-02-17 19:31:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,18 +5,41 @@ const footer = document.querySelector('footer')
 const showButton = document.querySelector('.menu-btn')
 const navMenu = document.querySelector('.nav-menu')
 const tecnologies = document.querySelectorAll('.fa-10x')
+const css = document.querySelector(".skill-css")
+const html = document.querySelector('.skill-html')
+const js = document.querySelector('.skill-js')
+const bootstrap = document.querySelector('.skill-bootstrap')
+const firstChild = document.querySelector('#container-info')
+const cards = document.querySelectorAll('.card')
+const profilePic = document.querySelector('#profile-pic')
+const title = document.querySelector('#nombre')
 
 darkModeButton.addEventListener('click', event => {
     header.classList.toggle('dark-theme')
     body.classList.toggle('dark-theme')
     footer.classList.toggle('dark-theme')
+    css.classList.toggle('dark-theme')
+    html.classList.toggle('dark-theme')
+    js.classList.toggle('dark-theme')
+    bootstrap.classList.toggle('dark-theme')
+    title.classList.toggle('dark-theme')
+    cards.forEach(card => {
+        card.classList.toggle('dark-theme')
+    })
+    profilePic.classList.toggle('dark-theme')
+    if(profilePic.classList.contains('dark-theme')){
+        profilePic.src = "./img/foto-byn.jpeg"
+    }else{
+        profilePic.src = "./img/foto-1.jpeg"
+    }
     if(header.classList.contains('dark-theme')){
         event.currentTarget.textContent = 'Dark Mode'
     }else{
         event.currentTarget.textContent = 'Light Mode'
     }
-})
+}) 
 
+    
 showButton.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
@@ -28,7 +51,18 @@ if(screen.width <= 480){
     })
 }
 
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= firstChild.offsetHeight){
+        css.classList.add('css')
+        html.classList.add('html')
+        js.classList.add('js')
+        bootstrap.classList.add('bootstrap')
+    }
+})
+
+
+
 ScrollReveal().reveal('#container-info');
 ScrollReveal().reveal('#tecnologies-container', {delay: 250});
-ScrollReveal().reveal('#projects-container', {delay: 250});
-ScrollReveal().reveal('#contact-container', {delay: 250});
+ScrollReveal().reveal('#projects-container', {delay: 500});
+ScrollReveal().reveal('#contact-container', {delay: 750});
